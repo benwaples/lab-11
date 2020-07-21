1827cf674d535708031ed55dcf98903179ec45b7
bwapes <benwaples@gmail.com>
2020-07-21 11:08:50 -0700

    updated master

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import { rawData } from './pokemon.js';
-import { getRandomPokemon, findById } from './utils.js';
+import { 
+    getRandomPokemon, 
+    findById,
+    getPokemonData,
+    addPokemonData
+} from './utils.js';
 
 // import functions and grab DOM elements
 const remainingTriesSpan = document.querySelector('#remaining-tries'); 
@@ -11,8 +16,6 @@ const seeResultsButton = document.querySelector('#see-results');
 // initialize state
 
 let remainingPokemon = rawData.slice();
-let capturedPokemon = [];
-let encounteredPokemon = [];
 let countCaptures = 10;
 
 resetPage();
@@ -21,31 +24,16 @@ function resetPage() {
     
     const pokemonPackage1 = getRandomPokemon(remainingPokemon);
     const randomPokemon1 = pokemonPackage1[1];
+    // checkEncountered(randomPokemon1, encounteredPokemon);
+
     const pokemonPackage2 = getRandomPokemon(pokemonPackage1[0]);
     const randomPokemon2 = pokemonPackage2[1];
+    // checkEncountered(randomPokemon2, encounteredPokemon);
+
     const pokemonPackage3 = getRandomPokemon(pokemonPackage2[0]);
     const randomPokemon3 = pokemonPackage3[1];
-
-    // add to encounter
-    const thisSetOfPokemon = [randomPokemon1, randomPokemon2, randomPokemon3];
-
-    for (let i = 0; i < thisSetOfPokemon.length; i++) {
-        const thisPokemon = thisSetOfPokemon[i];
-        const encounter = findById(encounteredPokemon, thisPokemon.id);
-        
-        if (!encounter) {
-            const initializePokemon = {
-                id: thisPokemon.id,
-                seen: 1
-            };
-            
-            encounteredPokemon.push(initializePokemon);
-        } else {
-            thisPokemon.seen++;
-        }
-    // send back to local storage here
-
-    }
+    // checkEncountered(randomPokemon3, encounteredPokemon);
+    // debugger
 
     const pokemonLabels = document.querySelectorAll('label');
     const pokemon1Label = pokemonLabels[0];
@@ -58,9 +46,9 @@ function resetPage() {
     pokemon1Input.addEventListener('click', selectedPokemon);
     const pokemon1img = pokemon1Label.children[1];
     pokemon1img.src = randomPokemon1.url_image;
-    const encounteredSpan1 = pokemon1Label.children[2];
-    const encounteredThis1 = findById(encounteredPokemon, randomPokemon1.id).seen;
-    encounteredSpan1.textContent = `Encounters: ${encounteredThis1}`;
+    // const encounteredSpan1 = pokemon1Label.children[2];
+    // const encounteredThis1 = findById(encounteredPokemon, randomPokemon1.id).seen;
+    // encounteredSpan1.textContent = `Encounters: ${encounteredThis1}`;
 
     //add the pokemon 2 to page
     const pokemon2Input = pokemon2Label.children[0];
@@ -68,9 +56,9 @@ function resetPage() {
     pokemon2Input.addEventListener('click', selectedPokemon);
     const pokemon2img = pokemon2Label.children[1];
     pokemon2img.src = randomPokemon2.url_image;
-    const encounteredSpan2 = pokemon2Label.children[2];
-    const encounteredThis2 =findById(encounteredPokemon, randomPokemon2.id).seen;
-    encounteredSpan2.textContent = `Encounters ${encounteredThis2}`;
+    // const encounteredSpan2 = pokemon2Label.children[2];
+    // const encounteredThis2 = findById(encounteredPokemon, randomPokemon2.id).seen;
+    // encounteredSpan2.textContent = `Encounters ${encounteredThis2}`;
 
     //add the pokemon 3 to page
     const pokemon3Input = pokemon3Label.children[0];
@@ -78,9 +66,9 @@ function resetPage() {
     pokemon3Input.addEventListener('click', selectedPokemon);
     const pokemon3img = pokemon3Label.children[1];
     pokemon3img.src = randomPokemon3.url_image;
-    const encounteredSpan3 = pokemon3Label.children[2];
-    const encounteredThis3 = findById(encounteredPokemon, randomPokemon3.id).seen;
-    encounteredSpan3.textContent = `Encounters ${encounteredThis3}`;
+    // const encounteredSpan3 = pokemon3Label.children[2];
+    // const encounteredThis3 = findById(encounteredPokemon, randomPokemon3.id).seen;
+    // encounteredSpan3.textContent = `Encounters ${encounteredThis3}`;
 
     pokemon1Input.disabled = false;
     pokemon2Input.disabled = false;
@@ -90,16 +78,24 @@ function resetPage() {
 }
 
 function selectedPokemon(e) {
-    seeResultsButton.parentElement.classList.remove('hidden');
     countCaptures--;
     
     const thisPokemon = e.target.value;
-    // debugger
-    const pokemonIndex = findById(remainingPokemon, thisPokemon);
-    
-    const addToCaptured = remainingPokemon.splice(pokemonIndex, 1);
-    
-    capturedPokemon.push(addToCaptured);
+
+    const data = getPokemonData();
+    const trackingPokemon = findById(remainingPokemon, Number(thisPokemon));
+
+    if (!trackingPokemon) {
+        const initialPokemon = {
+            id: thisPokemon.id,
+            captured: 1,
+        };
+        data.push(initialPokemon);
+    } else {
+        trackingPokemon.captured ++;
+    }
+
+    addPokemonData(data);
     
     if (countCaptures === 0) {
         alert('you\'re all out of pokeballs! Go to the next page to see your stats');
@@ -117,7 +113,7 @@ function selectedPokemon(e) {
     const pokemon1Input = pokemon1Label.children[0];
     pokemon1Input.disabled = true;
     const pokemon2Label = pokemonLabels[1];
-    const pokemon2Input = pokemon2Label.children[1];
+    const pokemon2Input = pokemon2Label.children[0];
     pokemon2Input.disabled = true;
     const pokemon3Label = pokemonLabels[2];
     const pokemon3Input = pokemon3Label.children[0];
@@ -131,9 +127,7 @@ function selectedPokemon(e) {
 nextButton.addEventListener('click', () => {
     resetPage();
 
-    console.log('encountered array', encounteredPokemon);
-    console.log('captured', capturedPokemon);
-    console.log('remaining poke', remainingPokemon);
+    // console.log('remaining poke', remainingPokemon);
 
     pokemonDiv.classList.remove('faded');
     nextButton.classList.add('hidden');
--- utils.js
+++ utils.js
@@ -1,9 +1,10 @@
+
+
 // function for getting random numbers between 0 and remaining pokemon length. whole numbers
 export function getRandomPokemon(pokemonArray) {
     let copyOfArray = pokemonArray.slice();
     const randomPokeIndex = Math.floor(Math.random() * copyOfArray.length);
-    const positionOneAhead = Number(randomPokeIndex) + 1;
-    copyOfArray.splice(randomPokeIndex, positionOneAhead);
+    copyOfArray.splice(randomPokeIndex, 1);
     const pokemonPackage = [copyOfArray, pokemonArray[randomPokeIndex]];
     return pokemonPackage;
 }
@@ -22,3 +23,17 @@ export function findById(someArray, someId) {
 //function for adding a selected pokemon to a new array
 
 //function for keeping count which pokemon have been shown and how many times.. Think incrementing the quantity
+
+export function getPokemonData(){
+    const initialEmpty = '[]';
+    const rawData = localStorage.getItem('DATA') || initialEmpty;
+    const data = JSON.parse(rawData);
+
+    return data;
+}
+
+export function addPokemonData(data) {
+    const stringyData = JSON.stringify(data);
+    localStorage.setItem('DATA', stringyData);
+}
+
